1595b43fd96dced0a1b159fc30fcaa714d7000e5
Rangana Withanage <rangana@rangana.de>
2019-05-22 00:05:59 +0200

    module-starter created content

---
+++ lib/Template/Plugin/Filter/PlantUML.pm
@@ -0,0 +1,108 @@
+package Template::Plugin::Filter::PlantUML;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Template::Plugin::Filter::PlantUML - The great new Template::Plugin::Filter::PlantUML!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Template::Plugin::Filter::PlantUML;
+
+    my $foo = Template::Plugin::Filter::PlantUML->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Rangana Sudesha Withanage, C<< <rwi at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-template-plugin-filter-plantuml at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Template-Plugin-Filter-PlantUML>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Template::Plugin::Filter::PlantUML
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Template-Plugin-Filter-PlantUML>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Template-Plugin-Filter-PlantUML>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Template-Plugin-Filter-PlantUML>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Template-Plugin-Filter-PlantUML>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is copyright (c) 2019 by Rangana Sudesha Withanage.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+
+=cut
+
+1; # End of Template::Plugin::Filter::PlantUML
